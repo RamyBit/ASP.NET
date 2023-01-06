19660c246efdc6929e010d1ae2caac5d99cd08b9
RamyBit <118202722+RamyBit@users.noreply.github.com>
2023-01-06 16:24:14 +0100

    Pary Invite 66

---
+++ FirstProject/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public string index()// IActionResult Index()
+        //{
+        //    return "Hello World"; //View();
+        //}
+         public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string text = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", text);
+        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
