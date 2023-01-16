7f7c5cea688eedc639bc784bcbe44d1bff8a14f5
RamyBit <118202722+RamyBit@users.noreply.github.com>
2023-01-16 15:35:28 +0100

    113

--- PSSolution/LanguageFeatures/Controllers/HomeController.cs
+++ PSSolution/LanguageFeatures/Controllers/HomeController.cs
@@ -11,15 +11,27 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            List<string> results = new List<string>();
-            foreach ( Product p in Product.GetProducts())
+            //List<string> results = new List<string>();
+            //foreach ( Product p in Product.GetProducts())
+            //{
+            //    string name = p?.Name ?? "<No Name>";
+            //    decimal? price = p?.Price ?? 0;
+            //    string realtedName = p?.Related?.Name ?? "<None>";
+            //    results.Add($"Name: {name}, Price: {price}, Related: {realtedName}");
+            //}
+            ShoppingCart cart
+                = new ShoppingCart { Products = Product.GetProducts() };
+
+            Product[] productArray =
             {
-                string name = p?.Name;
-                decimal? price = p?.Price;
-                string realtedName = p?.Related?.Name;
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, realtedName));
-            }
-            return View(results);
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M },
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            decimal cartTotal = cart.TotalPrices();
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
+            return View("Index", new string[] {$"Array Total:{arrayTotal:C2}"});
         }
     }
 }
